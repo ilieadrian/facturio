d655e6fc6e6fa8e7bd971bb3b89b25488bc88a3d
Ilie Adrian <ilie.adrian1986@gmail.com>
2020-05-22 15:30:05 +0300

    newsletter form finished

--- dist/scripts/validations.js
+++ dist/scripts/validations.js
@@ -1,15 +1,18 @@
+//footer newsletted signup form validation
+
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const emailVal = email.value;
-  console.log(email.value);
 
   if (!validateEmail(emailVal)) {
     form.classList.add('error');
   } else {
     form.classList.remove('error');
+    document.body.innerHTML = `<div style = "display:flex; align-items: center; 
+    justify-content: center; margin-top: 100px"><h2>Thank you for subscribing</h2></div>`;
   }
 });
 
