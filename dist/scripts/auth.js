20eb8776fb6c14321b64ba62f48923381c9aa2a0
Ilie Adrian <ilie.adrian1986@gmail.com>
2020-06-04 17:07:15 +0300

    Basic javascript form validation added

--- dist/scripts/auth.js
+++ dist/scripts/auth.js
@@ -1,29 +1,70 @@
-//Login-new account switcher
-const credentials = document.querySelector('.credentials');
-const btns = document.querySelectorAll('.tab-btn');
-const forms = document.querySelectorAll('.form-tab');
-
-credentials.addEventListener('click', function (e) {
-  const id = e.target.dataset.id;
-  if (id) {
-    // remove selected from other buttons
-    btns.forEach(function (btn) {
-      btn.classList.remove('active');
-    });
-    e.target.classList.add('active');
-    // hide other articles
-    forms.forEach(function (form) {
-      form.classList.remove('active');
-    });
-    const element = document.getElementById(id);
-    element.classList.add('active');
-  }
-});
-
 // Login-new account form data validate
-const form = document.getElementById('form-control');
+const form = document.querySelector('.form-tab');
+const username = document.getElementById('username');
+const fiscalCode = document.getElementById('fiscal-code');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+function checkUsername(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    validData(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+}
+
+function validData(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control';
+}
+
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      validData(input);
+    }
+  });
+}
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters long`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less then  ${max} characters`
+    );
+  } else {
+    validData(input);
+  }
+}
+
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
+
+function getFieldName(input) {
+  return input.name.charAt(0).toUpperCase() + input.name.slice(1);
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('asdas');
+
+  checkRequired([username, fiscalCode, password, password2]);
+  checkUsername(username);
 });
